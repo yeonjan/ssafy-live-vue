04a5e89bf09c0bc16b1e78ff6ec3099ef49d8da0
sxot94 <sxot94@gmail.com>
2022-11-21 17:51:58 +0900

    FEAT : APTLIST

--- src/store/modules/aptStore.js
+++ src/store/modules/aptStore.js
@@ -4,12 +4,22 @@ const aptStore = {
   namespaced: true,
   state: () => ({
     optionInfo: {}, // 시구군동 옵션 리스트를 담을 객체
+    aptDetailInfo: {}, // 아파트 상세 리스트를 받아올 객체
+    aptInfo: {}, //  아파트 리스트를 받아올 객체
   }),
   getters: {},
   mutations: {
     SET_OPTIONINFO(state, payload) {
       state.optionInfo = payload.optionInfo;
       console.log(state.optionInfo);
+    },
+    SET_APTDETAILINFO(state, payload) {
+      state.aptDetailInfo = payload.aptDetailInfo;
+      console.log(state.aptDetailInfo);
+    },
+    SET_APTINFO(state, payload) {
+      state.aptInfo = payload.aptInfo;
+      console.log(state.aptInfo);
     },
   },
   actions: {
@@ -19,6 +29,22 @@ const aptStore = {
 
       let optionInfo = data;
       commit("SET_OPTIONINFO", { optionInfo });
+    },
+
+    async aptList({ commit }, aptCodeInfo) {
+      console.log("aptList 실행");
+      let { data } = await http.post("/apts/", aptCodeInfo);
+
+      let aptInfo = data;
+      commit("SET_APTINFO", { aptInfo });
+    },
+
+    async aptDetailList({ commit }, regCodeInfo) {
+      console.log("aptDetailList 실행");
+      let { data } = await http.post("/apts/", regCodeInfo);
+
+      let aptDetailInfo = data;
+      commit("SET_APTDETAILINFO", { aptDetailInfo });
     },
   },
 };
--- src/views/Apt/AptNavigation.vue
+++ src/views/Apt/AptNavigation.vue
@@ -19,7 +19,7 @@
         </select>
       </div>
 
-      <div class="form-group col-md-2">
+      <!-- <div class="form-group col-md-2">
         <select class="form-select bg-dark text-light" id="year"></select>
       </div>
 
@@ -27,42 +27,12 @@
         <select class="form-select bg-dark text-light" id="month">
           <option value="">매매월선택</option>
         </select>
-      </div>
+      </div> -->
 
       <div class="form-group col-md-2">
-        <button type="button" id="list-btn" class="btn btn-outline-primary">검색</button>
+        <button type="button" id="list-btn" class="btn btn-outline-primary" @click="searchApt()">검색</button>
       </div>
     </div>
-    <!-- <div class="map_main_container" style="display: none">
-      <div style="height: 70px"></div>
-
-      <div class="alert alert-primary mt-3"><strong>국토교통부</strong> 지역별 아파트매매 거래 정보</div>
-
-      <div class="row col-md-12">
-        <div class="form-group ms-2 mb-2 text-end">
-          <button type="button" id="btn-list" class="btn btn-outline-danger">아파트 정보</button>
-        </div>
-      </div>
-    </div> -->
-    <!-- <table class="table table-hover text-center" style="display: none">
-      <tr>
-        <th>즐겨찾기</th>
-
-        <th>아파트이름</th>
-
-        <th>층</th>
-
-        <th>면적</th>
-
-        <th>법정동</th>
-
-        <th>거래금액</th>
-
-        <th style="display: none">aptNo</th>
-      </tr>
-
-      <tbody id="aptlist"></tbody>
-    </table> -->
   </div>
 </template>
 
@@ -70,32 +40,62 @@
 import store from "@/store";
 
 export default {
+  data() {
+    return {
+      aptInfo: {},
+      aptDetailInfo: {},
+    };
+  },
+
+  methods: {
+    async searchApt() {
+      let dongSel = document.querySelector("#dong");
+      let regCodeInfo = dongSel[dongSel.selectedIndex].value;
+      let aptInfo = {
+        regcode: regCodeInfo,
+      };
+      await store.dispatch("aptStore/aptList", aptInfo);
+      this.aptInfo = store.state.aptStore.aptInfo;
+      console.log(this.aptInfo);
+    },
+
+    async searchDetailApt() {
+      let dongSel = document.querySelector("#dong");
+      let regCodeInfo = dongSel[dongSel.selectedIndex].value;
+      let aptDetailInfo = {
+        regcode: regCodeInfo,
+      };
+      await store.dispatch("aptStore/aptDetailList", aptDetailInfo);
+      this.aptDetailInfo = store.state.aptStore.aptDetailInfo;
+      console.log(this.aptDetailInfo);
+    },
+  },
   mounted() {
-    let date = new Date();
+    // let date = new Date();
     window.onload = function () {
-      let yearEl = document.querySelector("#year");
-      let yearOpt = `<option value="">매매년도선택</option>`;
-      let year = date.getFullYear();
-      for (let i = year; i > year - 20; i--) {
-        yearOpt += `<option value="${i}">${i}년</option>`;
-      }
-      yearEl.innerHTML = yearOpt;
+      //   let yearEl = document.querySelector("#year");
+      //   let yearOpt = `<option value="">매매년도선택</option>`;
+      //   let year = date.getFullYear();
+      //   for (let i = year; i > year - 20; i--) {
+      //     yearOpt += `<option value="${i}">${i}년</option>`;
+      //   }
+      //   yearEl.innerHTML = yearOpt;
 
       // 브라우저가 열리면 시도정보 얻기.
       sendRequest("sido", "*00000000", "sido");
     };
 
-    document.querySelector("#year").addEventListener("change", function () {
-      let month = date.getMonth() + 1;
-      let monthEl = document.querySelector("#month");
-      let monthOpt = `<option value="">매매월선택</option>`;
-      let yearSel = document.querySelector("#year");
-      let m = yearSel[yearSel.selectedIndex].value == date.getFullYear() ? month : 13;
-      for (let i = 1; i < m; i++) {
-        monthOpt += `<option value="${i < 10 ? "0" + i : i}">${i}월</option>`;
-      }
-      monthEl.innerHTML = monthOpt;
-    });
+    // document.querySelector("#year").addEventListener("change", function () {
+    //   let month = date.getMonth() + 1;
+    //   let monthEl = document.querySelector("#month");
+    //   let monthOpt = `<option value="">매매월선택</option>`;
+    //   let yearSel = document.querySelector("#year");
+    //   let m = yearSel[yearSel.selectedIndex].value == date.getFullYear() ? month : 13;
+    //   for (let i = 1; i < m; i++) {
+    //     monthOpt += `<option value="${i < 10 ? "0" + i : i}">${i}월</option>`;
+    //   }
+    //   monthEl.innerHTML = monthOpt;
+    // });
 
     // 시도가 바뀌면 구군정보 얻기.
     document.querySelector("#sido").addEventListener("change", function () {
