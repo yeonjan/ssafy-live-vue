14f8c35fe15cfe862ada1fcd444c75d2577104ea
y_jin99 <y_jin99@naver.com>
2022-11-24 12:25:52 +0900

    FEAT : 가격 그래프

---
+++ src/components/chart/LineChart.vue
@@ -0,0 +1,39 @@
+<script>
+import { Line } from "vue-chartjs/legacy"; // 2버전
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+} from "chart.js";
+
+ChartJS.register(Title, Tooltip, Legend, CategoryScale, LinearScale, PointElement, LineElement);
+// import { Chart, registerables } from 'chart.js'
+// Chart.register(...registerables)
+
+export default {
+  extends: Line,
+  props: {
+    chartData: {
+      type: Object,
+    },
+    options: {
+      type: Object,
+    },
+    width: {
+      type: Number,
+      default: 400,
+    },
+    height: {
+      type: Number,
+      default: 400,
+    },
+  },
+};
+</script>
+
+<style></style>
